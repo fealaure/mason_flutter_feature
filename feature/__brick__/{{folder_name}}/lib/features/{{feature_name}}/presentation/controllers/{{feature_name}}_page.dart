8dac21cd38aea794e9cbc24639cdb348f773a51a
Marcio <marcio.lorran@gmail.com>
2025-02-12 16:49:58 -0300

    set the correct lambdas

--- feature/__brick__/{{folder_name}}/lib/features/{{feature_name}}/presentation/controllers/{{feature_name}}_page.dart
+++ feature/__brick__/{{folder_name}}/lib/features/{{feature_name}}/presentation/controllers/{{feature_name}}_page.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
 
-class {{feature_name.pascalCase()}}View extends HookConsumerWidget {
-  const {{feature_name.pascalCase()}}View({Key? key}) : super(key: key);
+class {{feature_name.pascalCase}}View extends HookConsumerWidget {
+  const {{feature_name.pascalCase}}View({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final {{feature_name.pascalCase()}}State state = ref.watch({{feature_name.pascalCase()}}Provider);
-    final {{feature_name.pascalCase()}}Controller controller = ref.read({{feature_name.pascalCase()}}Provider.notifier);
+    final {{feature_name.pascalCase}}State state = ref.watch({{feature_name.camelCase}}Provider);
+    final {{feature_name.pascalCase}}Controller controller = ref.read({{feature_name.camelCase}}Provider.notifier);
 
     return Scaffold(
       appBar: AppBar(
-          title: {{feature_name}},
+          title: '{{feature_name.pascalCase}}',
           onBackButtonPressed: () {
             context.pop();
           },
         ),
       body: Center(
-        child: Text('This is the {{feature_name.pascalCase()}} view'),
+        child: Text('This is the {{feature_name.pascalCase}} view'),
       ),
     );
   }
