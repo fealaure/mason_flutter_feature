20ecf812caed6d4840cbc1dbe185509716972226
Marcio <marcio.lorran@gmail.com>
2025-02-12 18:22:33 -0300

    fix provider and add WidgetRef to the view

--- feature/__brick__/{{folder_name}}/lib/features/{{feature_name}}/presentation/controllers/common/{{feature_name}}_providers.dart
+++ feature/__brick__/{{folder_name}}/lib/features/{{feature_name}}/presentation/controllers/common/{{feature_name}}_providers.dart
@@ -1,3 +1,3 @@
-final {{feature_name.camelCase()}}NotifierProvider = StateNotifierProvider<{{feature_name.pascalCase()}}Notifier, void>((ref) {
+final {{feature_name.camelCase()}}NotifierProvider = StateNotifierProvider<{{feature_name.pascalCase()}}Notifier, {{feature_name.pascalCase()}}State>((ref) {
   return {{feature_name.pascalCase()}}Notifier(ref.container);
 });
--- feature/__brick__/{{folder_name}}/lib/features/{{feature_name}}/presentation/controllers/{{feature_name}}_page.dart
+++ feature/__brick__/{{folder_name}}/lib/features/{{feature_name}}/presentation/controllers/{{feature_name}}_page.dart
@@ -4,16 +4,13 @@ class {{feature_name.pascalCase()}}View extends HookConsumerWidget {
   const {{feature_name.pascalCase()}}View({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final {{feature_name.pascalCase()}}State state = ref.watch({{feature_name.camelCase()}}Provider);
     final {{feature_name.pascalCase()}}Controller controller = ref.read({{feature_name.camelCase()}}Provider.notifier);
 
     return Scaffold(
       appBar: AppBar(
           title: '{{feature_name.pascalCase()}}',
-          onBackButtonPressed: () {
-            context.pop();
-          },
         ),
       body: Center(
         child: Text('This is the {{feature_name.pascalCase()}} view'),
