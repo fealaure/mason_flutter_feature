1627a401dd0d81cf86a691cbf85df42682c74b25
Marcio <marcio.lorran@gmail.com>
2025-02-12 18:53:58 -0300

    add initialize handler and load strings method

--- feature/__brick__/{{folder_name}}/lib/features/{{feature_name}}/presentation/controllers/{{feature_name}}_notifier.dart
+++ feature/__brick__/{{folder_name}}/lib/features/{{feature_name}}/presentation/controllers/{{feature_name}}_notifier.dart
@@ -1,5 +1,7 @@
 class {{feature_name.pascalCase()}}Notifier extends BaseStateNotifier<{{feature_name.pascalCase()}}State> {
   final ProviderContainer container;
+  ApiHandler? _apiHandler;
+  Map<dynamic, String>? _strings;
   
   {{feature_name.pascalCase()}}Notifier(this.container) : super(const {{feature_name.pascalCase()}}State()) {
     _initializeApiHandler();
@@ -9,10 +11,37 @@ class {{feature_name.pascalCase()}}Notifier extends BaseStateNotifier<{{feature_
   }
 
   Future<void> _loadStrings() async {
-    //TODO: Load strings
+    final stringsNotifier = container.read(allStringsNotifierProvider.notifier);
+    await stringsNotifier.loadStrings();
+    if (!stringsNotifier.mounted) return;
+
+    final loadedStrings = stringsNotifier.state;
+    final updatedStrings = {
+      ...?_strings,
+      ...Map.fromEntries(
+        FirestoreKeys.values.where((key) => loadedStrings.containsKey(key)).map((key) => MapEntry(key, loadedStrings[key]!)),
+      ),
+    };
+
+    _strings = updatedStrings;
+    _updateStateWithStrings();
+  }
+
+  void _updateStateWithStrings() {
+    safeUpdate(() => state = state.copyWith(strings: _strings));
   }
 
   Future<void> _initializeApiHandler() async {
-    //TODO: Initialize API handler
+    _apiHandler = await container.read(apiHandlerProvider.future);
+    container.listen<AsyncValue<ApiHandler>>(
+      apiHandlerProvider,
+      (_, apiHandlerValue) {
+        if (apiHandlerValue.hasValue) {
+          _apiHandler = apiHandlerValue.value!;
+        } else if (apiHandlerValue.hasError) {
+          debugPrint("Error loading ApiHandler: ${apiHandlerValue.error}");
+        }
+      },
+    );
   }
 }
